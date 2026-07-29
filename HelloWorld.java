bf6dcbefd305c3275417f9a14b2f349c12c7e9b6
parasbane <parasbane1106@gmail.com>
2026-07-29 12:02:42 +0530

    Create HelloWorld.java
    Hello World Code, By Paras !

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+//Coded By Paras The Great!
+
+//class hello{public static void main(args)
+//          System.out.println("Hello World")
+           }
+//till here.
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
